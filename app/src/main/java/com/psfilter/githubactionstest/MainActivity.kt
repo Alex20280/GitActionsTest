6728ddc5dbac80401b985bb0ddf686688b5ad24e
Alex <aleksandrbasanets2012@gmail.com>
2024-11-01 15:46:42 +0200

    made mistakes 2

--- app/src/main/java/com/psfilter/githubactionstest/MainActivity.kt
+++ app/src/main/java/com/psfilter/githubactionstest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.psfilter.githubactionstest
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,15 +9,17 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-
-             override fun onCreate(savedInstanceState: Bundle?) {
+                override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+                    Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
     }
 }
