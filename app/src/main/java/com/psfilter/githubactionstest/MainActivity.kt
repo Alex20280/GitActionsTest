afe93d8790d58e3ea0da685335cf780a4f07434a
Alex <aleksandrbasanets2012@gmail.com>
2024-11-09 14:48:25 +0200

    test 4 branch

--- app/src/main/java/com/psfilter/githubactionstest/MainActivity.kt
+++ app/src/main/java/com/psfilter/githubactionstest/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Made some functionality for second branch", Toast.LENGTH_SHORT).show()
         Toast.makeText(this, "Made some functionality for 3 branch", Toast.LENGTH_SHORT).show()
         Toast.makeText(this, "Made some functionality for 3 branch", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Made some functionality for 4 branch", Toast.LENGTH_SHORT).show()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
